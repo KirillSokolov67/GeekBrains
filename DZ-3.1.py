08f0245e4aa0d2d55e055bd153192f45a531d9d2
Кирилл <kostagood213@mail.ru>
2023-05-08 00:57:25 +0300

    Домашки 3 урока

---
+++ DZ-3.1.py
@@ -0,0 +1,7 @@
+def calculator(a, b):
+    try:
+        return a/b
+    except ZeroDivisionError as e:
+        print(f'Ошибка ')
+
+print(calculator(int(input('Первое число: ')), int(input('Второе число: '))))
